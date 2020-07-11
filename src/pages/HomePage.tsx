bd85ce472fb5a50b1330d4638a7603b4a5d69ff6
h-github <hmohammadi@hotmail.ca>
2020-07-10 23:11:04 -0700

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Welcome to my blog!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+        scelerisque aliquam pellentesque. Morbi non interdum libero. Phasellus
+        fermentum suscipit lectus, non molestie urna dictum nec. Curabitur
+        vehicula ante nisi, sed vehicula leo congue non. Donec aliquam sit amet
+        nisi quis sodales. Phasellus ac pellentesque nulla. Maecenas non
+        porttitor ipsum. Praesent non turpis id arcu sagittis porttitor.
+        Suspendisse in velit vel ante laoreet consectetur a ut risus.
+        Suspendisse a nisi nec dolor commodo luctus. Duis efficitur arcu a
+        dignissim porttitor. Vivamus nec ipsum velit. Morbi pellentesque sodales
+        risus et varius. Sed tristique nec arcu vel rutrum. Duis ultrices arcu
+        id ligula venenatis commodo. Donec rhoncus iaculis consectetur.
+      </p>
+      <p>
+        Quisque varius, ipsum sed feugiat auctor, ipsum nisl aliquet purus, quis
+        cursus diam purus non nibh. Aliquam felis arcu, bibendum eget auctor sit
+        amet, interdum at est. Etiam ut est pulvinar massa viverra aliquet.
+        Mauris urna mauris, sagittis sit amet mauris vitae, accumsan ultrices
+        tortor. Vivamus molestie consectetur metus nec posuere. Sed ut velit nec
+        velit bibendum aliquet. Maecenas eros metus, finibus et rutrum ut,
+        venenatis et erat.
+      </p>
+      <p>
+        Vivamus in ultrices nunc, eget aliquet felis. Etiam posuere tincidunt
+        enim, sagittis maximus quam dignissim at. Donec convallis dictum nulla,
+        ut aliquam risus facilisis et. Cras lacinia, justo in facilisis
+        tristique, leo felis ornare orci, eu pulvinar est magna sit amet eros.
+        Sed sit amet purus at ex varius lacinia pharetra in nisl. Ut mollis enim
+        non augue varius semper. Ut dui quam, placerat eu dolor eu, semper
+        mattis leo. Duis mi sapien, pellentesque at ligula eu, aliquet fermentum
+        arcu. Nam sem dui, tristique sed finibus vel, aliquet vel ante.
+        Vestibulum pulvinar, nisi ut sodales mollis, arcu dolor mollis turpis,
+        et tincidunt justo erat eu dui. Phasellus neque odio, maximus at mattis
+        quis, malesuada nec lorem. In mauris dolor, ultrices sed sapien sed,
+        venenatis posuere erat. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Maecenas eu iaculis est.
+      </p>
+      <p>
+        Morbi gravida consequat turpis, ut egestas mauris ultricies eget. Cras
+        sed varius enim. Suspendisse tellus nibh, varius id euismod quis,
+        placerat sit amet nulla. Suspendisse tristique, justo sit amet
+        consectetur laoreet, eros velit feugiat lorem, eget posuere magna urna
+        ac risus. Suspendisse potenti. Aliquam gravida elit auctor, porttitor
+        neque vitae, cursus nulla. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos.
+      </p>
+      <p>
+        In purus magna, dignissim quis porttitor sed, rutrum sed lacus. Aliquam
+        sit amet lacus sed augue hendrerit mattis. Integer dapibus turpis nisl.
+        Nullam vehicula ex dictum, pellentesque mi vel, volutpat massa.
+        Pellentesque sodales tortor massa, vel elementum mi porta vitae. In quis
+        purus ut tellus blandit placerat a quis lorem. Maecenas bibendum ipsum
+        velit. Phasellus maximus finibus nunc, nec pretium est efficitur eget.
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
