af5c49cf4b6ec7228b229da3ce0c4bfdbdb8b659
h-github <hmohammadi@hotmail.ca>
2020-07-12 00:58:30 -0700

    Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <div className="container">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesListPage} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/ArticlesListPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ArticlesListPage = () => {
+  return (
+    <>
+      <h1>Articles</h1>
+    </>
+  );
+};
+
+export default ArticlesListPage;
